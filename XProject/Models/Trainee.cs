d9f8d5220b00d3626706ac42dfcbfae189735a60
Abdulaziz Alhlwah <rhyfmedia@gmail.com>
2021-06-27 00:37:44 +0300

    Add files via upload

--- XProject/Models/Trainee.cs
+++ XProject/Models/Trainee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace XProject.Models
     public class Trainee
     {
         public int ID { get; set; }
+      
         public int nationalId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
