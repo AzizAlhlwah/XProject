bc039be98dfd20031e99b6e5931f0cf97797a870
AzizAlhlwah <rhyfmedia@gmail.com>
2021-07-01 23:09:26 +0300

    Coach Contaraller

--- XProject/Controllers/CoachController.cs
+++ XProject/Controllers/CoachController.cs
@@ -1,4 +1,3 @@
-﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +28,7 @@ namespace XProject.Controllers
 
         public IActionResult Index()
         {
-            if(ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
+            if (ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
             {
                 return RedirectToAction("login", "Home");
             }
@@ -51,6 +50,22 @@ namespace XProject.Controllers
         }
 
         public IActionResult MyProfile()
+        {
+            if (ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("login", "Home");
+            }
+
+            var NID =Convert.ToInt32(_httpContextAccessor.HttpContext
+                            .Session.GetString("UserId"));
+
+            var result = _context.applactionUsers.Where
+                            (x => x.nationalId == NID).ToList();
+            ViewBag.InfoCoach = result;
+            return View();
+        }
+
+        public IActionResult Settings()
         {
             if (ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
             {
@@ -88,101 +103,7 @@ namespace XProject.Controllers
         //    return View();
         //}
 
-        
+
 
     }
-
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using XProject.Models;
-
-namespace XProject.Controllers
-{
-    public class CoachController : Controller
-    {
-        private readonly DBContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public CoachController(ILogger<CoachController> logger, DBContext context,
-            IWebHostEnvironment hostingEnvironment
-            , IHttpContextAccessor httpContextAccessor)
-        {
-            _context = context;
-            _webHostEnvironment = hostingEnvironment;
-            _httpContextAccessor = httpContextAccessor;
-        }
-
-
-        public IActionResult Index()
-        {
-            if(ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
-            {
-                return RedirectToAction("login", "Home");
-            }
-
-            var result = _context.applactionUsers.Where(x => x.nationalId != null);
-
-            return View(result);
-        }
-
-
-        public IActionResult dashboard()
-        {
-            if (ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
-            {
-                return RedirectToAction("login", "Home");
-            }
-
-            return View();
-        }
-
-        public IActionResult MyProfile()
-        {
-            if (ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId") == null)
-            {
-                return RedirectToAction("login", "Home");
-            }
-            return View();
-        }
-
-
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //public IActionResult Create(Coach model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Coach c = new Coach();
-        //        c.Name = model.Name;
-        //        c.nationalId = model.nationalId;
-        //        c.Mobile = model.Mobile;
-        //        c.DesiredPeriod = model.DesiredPeriod;
-        //        c.DesiredTime = model.DesiredTime;
-        //        c.Email = model.Email;
-
-        //        _context.coach.Add(c);
-        //         var result = _context.SaveChanges();
-
-        //        return RedirectToAction("Index");
-
-        //    }
-
-        //    return View();
-        //}
-
-        
-
-    }
-
 }
--- XProject/Controllers/HomeController.cs
+++ XProject/Controllers/HomeController.cs
@@ -23,13 +23,13 @@ namespace XProject.Controllers
 
         public HomeController(ILogger<HomeController> logger, DBContext context,
                 IWebHostEnvironment hostingEnvironment
-                ,IHttpContextAccessor httpContextAccessor)
-            {
-                _context = context;
-                _logger = logger;
-                _httpContextAccessor = httpContextAccessor;
+                , IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
 
-            
+
         }
 
         public IActionResult Index()
@@ -57,10 +57,10 @@ namespace XProject.Controllers
         }
 
 
-       
+
         public IActionResult register()
         {
-            
+
 
             if (IsLogin())
             {
@@ -75,12 +75,13 @@ namespace XProject.Controllers
         public IActionResult register(ApplactionUsers user)
         {
 
-            if(user.Password != user.Confirmpassword)
+            if (user.Password != user.Confirmpassword)
             {
                 ViewBag.Error = " كلمة المرور وتاكيد كلمة المرور لاتتطابق   ";
 
             }
-            else { 
+            else
+            {
 
                 _context.applactionUsers.Add(user);
                 int result = _context.SaveChanges();
@@ -107,27 +108,29 @@ namespace XProject.Controllers
         [HttpPost]
         public IActionResult login(ApplactionUsers user)
         {
-           var Result = _context.applactionUsers.Where(x => x.Email == user.Email &&
-                                           x.Password == user.Password).FirstOrDefault();
+            var Result = _context.applactionUsers.Where(x => x.Email == user.Email &&
+                                            x.Password == user.Password).FirstOrDefault();
             if (Result != null)
             {
-                _httpContextAccessor.HttpContext.Session.SetString("UserId",user.nationalId.ToString());
+                _httpContextAccessor.HttpContext.Session.SetString("UserId", Result.nationalId.ToString());
                 var roll = Result.rolls;
-               _httpContextAccessor.HttpContext.Session.SetString("Roll", roll);
+                _httpContextAccessor.HttpContext.Session.SetString("Roll", roll);
 
-                if(roll == "1")
+                if (roll == "1")
                 {
                     return RedirectToAction("dashboard", "Coach");
 
-                } else if (roll == "2"){
+                }
+                else if (roll == "2")
+                {
                     return RedirectToAction("dashboard", "Trainee");
                 }
-                else if(roll == "3")
+                else if (roll == "3")
                 {
                     return RedirectToAction("dashboard", "Admin");
                 }
 
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
 
             ViewBag.result = " <h6> البريد الإلكتروني أو كلمة المرور خاطئة </h6>";
@@ -142,13 +145,13 @@ namespace XProject.Controllers
 
         public IActionResult RedirectBaesOnRoll()
         {
-         
+
             ViewBag.roll = _httpContextAccessor.HttpContext.Session.GetString("Roll");
             if (ViewBag.roll == "1")
             {
                 return RedirectToAction("dashboard", "Coach");
             }
-            else if  (ViewBag.roll == "2")
+            else if (ViewBag.roll == "2")
             {
                 return RedirectToAction("dashboard", "Trainee");
             }
@@ -160,188 +163,7 @@ namespace XProject.Controllers
             return RedirectToAction("Index", "Home");
 
         }
-        
 
-        public bool IsLogin()
-        {
-            var Nid = _httpContextAccessor.HttpContext.Session.GetString("UserId");
-            if (Nid != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-
-    }
-}
-
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using XProject.Models;
-using Microsoft.AspNetCore.Http;
-using XProject.libraries;
-
-namespace XProject.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly DBContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        //utilities uti = new utilities();
-
-        public HomeController(ILogger<HomeController> logger, DBContext context,
-                IWebHostEnvironment hostingEnvironment
-                ,IHttpContextAccessor httpContextAccessor)
-            {
-                _context = context;
-                _logger = logger;
-                _httpContextAccessor = httpContextAccessor;
-
-            
-        }
-
-        public IActionResult Index()
-        {
-            // Git
-            ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId");
-
-            //Set
-            //_httpContextAccessor.HttpContext.Session.SetString("UserId", data.emp_id.ToString());
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            // Git
-            ViewBag.NID = _httpContextAccessor.HttpContext.Session.GetString("UserId");
-
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-
-       
-        public IActionResult register()
-        {
-            
-
-            if (IsLogin())
-            {
-                // You Aready login
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult register(ApplactionUsers user)
-        {
-
-            if(user.Password != user.Confirmpassword)
-            {
-                ViewBag.Error = " كلمة المرور وتاكيد كلمة المرور لاتتطابق   ";
-
-            }
-            else { 
-
-                _context.applactionUsers.Add(user);
-                int result = _context.SaveChanges();
-
-                if (result == 1)
-                {
-                    ViewBag.result = "<strong>تم تسجيلك بنجاح!</strong>";
-                }
-            }
-            return View("register");
-        }
-
-        public IActionResult login()
-        {
-            if (IsLogin())
-            {
-                // You Aready login
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult login(ApplactionUsers user)
-        {
-           var Result = _context.applactionUsers.Where(x => x.Email == user.Email &&
-                                           x.Password == user.Password).FirstOrDefault();
-            if (Result != null)
-            {
-                _httpContextAccessor.HttpContext.Session.SetString("UserId",user.nationalId.ToString());
-                var roll = Result.rolls;
-               _httpContextAccessor.HttpContext.Session.SetString("Roll", roll);
-
-                if(roll == "1")
-                {
-                    return RedirectToAction("dashboard", "Coach");
-
-                } else if (roll == "2"){
-                    return RedirectToAction("dashboard", "Trainee");
-                }
-                else if(roll == "3")
-                {
-                    return RedirectToAction("dashboard", "Admin");
-                }
-
-                return RedirectToAction("Index","Home");
-            }
-
-            ViewBag.result = " <h6> البريد الإلكتروني أو كلمة المرور خاطئة </h6>";
-
-            return View();
-        }
-        public IActionResult logout()
-        {
-            _httpContextAccessor.HttpContext.Session.Clear();
-            return RedirectToAction("login");
-        }
-
-        public IActionResult RedirectBaesOnRoll()
-        {
-         
-            ViewBag.roll = _httpContextAccessor.HttpContext.Session.GetString("Roll");
-            if (ViewBag.roll == "1")
-            {
-                return RedirectToAction("dashboard", "Coach");
-            }
-            else if  (ViewBag.roll == "2")
-            {
-                return RedirectToAction("dashboard", "Trainee");
-            }
-            else
-            {
-                return RedirectToAction("dashboard", "Admin");
-            }
-
-            return RedirectToAction("Index", "Home");
-
-        }
-        
 
         public bool IsLogin()
         {
